1ddc3db5cc619391739816a01e120e3df83ae77d
chandanch <chandan.cec107@gmail.com>
2024-08-25 05:40:10 +0530

    test name and slug length

--- app/models.py
+++ app/models.py
@@ -14,8 +14,8 @@ class Category(Base):
     __tablename__ = "category"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(255), nullable=False)
-    slug = Column(String(120), nullable=False)
+    name = Column(String(100), nullable=False)
+    slug = Column(String(140), nullable=False)
     is_active = Column(Boolean, nullable=False, default=False, server_default="False")
     level = Column(Integer, nullable=False, default="100", server_default="100")
     parent_id = Column(Integer, nullable=True)

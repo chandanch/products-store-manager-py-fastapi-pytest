795e501857058a0b2f6188a0e95393e1a54fbae1
chandanch <chandan.cec107@gmail.com>
2024-08-30 10:44:41 +0530

    update column types test to match column names

--- tests/models/test_product.py
+++ tests/models/test_product.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Integer, String, Boolean
-
+from sqlalchemy import Integer, String, Boolean, Text, DateTime, Enum
+from sqlalchemy.dialects.postgresql import UUID
 
 TABLE_NAME = "product"
 
@@ -14,10 +14,16 @@ def test_model_structure_product_column_types(db_inspector):
     }
 
     assert isinstance(columns["id"]["type"], Integer)
+    assert isinstance(columns["pid"]["type"], UUID)
     assert isinstance(columns["name"]["type"], String)
     assert isinstance(columns["slug"]["type"], String)
+    assert isinstance(columns["description"]["type"], Text)
+    assert isinstance(columns["is_digital"]["type"], Boolean)
+    assert isinstance(columns["created_at"]["type"], DateTime)
+    assert isinstance(columns["updated_at"]["type"], DateTime)
     assert isinstance(columns["is_active"]["type"], Boolean)
-    assert isinstance(columns["level"]["type"], Integer)
-    assert isinstance(columns["parent_id"]["type"], Integer)
+    assert isinstance(columns["stock_status"]["type"], Enum)
+    assert isinstance(columns["category_id"]["type"], Integer)
+    assert isinstance(columns["seasonal_event"]["type"], Integer)
 
     # print(columns)

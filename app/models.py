74bb601cb58fceb753cf2161025bc97980ecfc10
chandanch <chandan.cec107@gmail.com>
2024-08-30 13:17:08 +0530

    model product table columns

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ from sqlalchemy import (
     Boolean,
     CheckConstraint,
     UniqueConstraint,
+    Text,
+    DateTime,
+    text,
+    func,
+    Enum,
+    ForeignKey,
 )
 
 from app.settings.db_connection import Base
@@ -25,7 +31,7 @@ class Category(Base):
     slug = Column(String(140), nullable=False)
     is_active = Column(Boolean, nullable=False, default=False, server_default="False")
     level = Column(Integer, nullable=False, default="100", server_default="100")
-    parent_id = Column(Integer, nullable=True)
+    parent_id = Column(Integer, ForeignKey("category.id"), nullable=True)
 
     __tableargs__ = (
         CheckConstraint("LENGTH(name) > 0", name="name_length_constraint"),
@@ -33,3 +39,36 @@ class Category(Base):
         UniqueConstraint("name", "level", name="unq_category_name_level_constraint"),
         UniqueConstraint("slug", name="unq_category_slug_constraint"),
     )
+
+
+class Product(Base):
+
+    __tablename__ = "product"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(200), nullable=False)
+    slug = Column(String(220), nullable=False)
+    description = Column(Text, nullable=True)
+    is_digital = Column(Boolean, nullable=False, default=False, server_default="False")
+    created_at = Column(
+        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP"),
+        onupdate=func.now(),
+    )
+    is_active = Column(Boolean, nullable=False, default=False, server_default="False")
+    stock_status = Column(
+        Enum(
+            "outofstock",
+            "available",
+            "ordered",
+            name="status_enum",
+        ),
+        nullable=False,
+        server_default="outofstock",
+    )
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+    seasonal_event = Column(Integer, ForeignKey("seasonal_event.id"), nullable=False)

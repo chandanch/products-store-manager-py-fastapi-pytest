2406411b419e8cd08f6a0556d4aaec443862692d
chandanch <chandan.cec107@gmail.com>
2024-08-30 10:23:35 +0530

    rename tests to product model

--- tests/models/test_product.py
+++ tests/models/test_product.py
@@ -4,11 +4,11 @@ from sqlalchemy import Integer, String, Boolean
 TABLE_NAME = "product"
 
 
-def test_model_structure_category_table_exists(db_inspector):
+def test_model_structure_product_table_exists(db_inspector):
     assert db_inspector.has_table(TABLE_NAME)
 
 
-def test_model_structure_category_column_types(db_inspector):
+def test_model_structure_product_column_types(db_inspector):
     columns = {
         columns["name"]: columns for columns in db_inspector.get_columns(TABLE_NAME)
     }

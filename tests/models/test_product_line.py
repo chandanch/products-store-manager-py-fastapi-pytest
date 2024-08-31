821286e0992a4d783c12319379492e676dad765e
chandanch <chandan.cec107@gmail.com>
2024-08-31 11:51:30 +0530

    add tests for constraints and default values

--- tests/models/test_product_line.py
+++ tests/models/test_product_line.py
@@ -22,3 +22,57 @@ def test_model_structure_product_line_column_types(db_inspector):
     assert isinstance(columns["order"]["type"], Integer)
     assert isinstance(columns["weight"]["type"], Float)
     assert isinstance(columns["product_id"]["type"], Integer)
+
+
+def test_model_structure_product_nullable_constraints(db_inspector):
+    columns = db_inspector.get_columns(TABLE_NAME)
+
+    expected_nullable_columns = {
+        "id": False,
+        "price": False,
+        "sku": False,
+        "stock_qty": False,
+        "is_active": False,
+        "order": False,
+        "weight": False,
+        "product_id": False,
+    }
+
+    for column in columns:
+        # print(f"Column: {column}")
+        # Raise assertion error with an optional error message
+        assert column["nullable"] == expected_nullable_columns.get(
+            column["name"]
+        ), f"Column '{column['name']}' cannot be null"
+
+
+def test_model_structure_product_line_column_constraints(db_inspector):
+    constraints = db_inspector.get_check_constraints(TABLE_NAME)
+
+    for constraint in constraints:
+        assert any(constraint["order"] == "product_line_order_range_constraint")
+        assert any(constraint["price"] == "product_line_price_max_constraint")
+
+
+def test_model_structure_product_line_default_values(db_inspector):
+    columns = {
+        columns["name"]: columns for columns in db_inspector.get_columns(TABLE_NAME)
+    }
+    # print(columns)
+    assert columns["stock_qty"]["default"] == "0"
+    assert columns["is_active"]["default"] == "false"
+    assert columns["stock_status"]["default"] == "'outofstock'::status_enum"
+
+
+def test_model_structure_product_unique_constraints(db_inspector):
+    constraints = db_inspector.get_unique_constraints(TABLE_NAME)
+    # print("Test Constraints...", constraints)
+
+    assert any(
+        constraint["name"] == "unq_product_line_sku_constraint"
+        for constraint in constraints
+    )
+    assert any(
+        constraint["name"] == "unq_product_line_order_product_id_constraint"
+        for constraint in constraints
+    )

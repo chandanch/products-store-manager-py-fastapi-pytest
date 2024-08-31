dfbd87132c3e149170525677d398a9494d0d9fb4
chandanch <chandan.cec107@gmail.com>
2024-08-31 11:32:19 +0530

    test column types in product_line

--- tests/models/test_product_line.py
+++ tests/models/test_product_line.py
@@ -1,5 +1,24 @@
+from sqlalchemy import Integer, Boolean, Numeric, Float
+from sqlalchemy.dialects.postgresql import UUID
+
+
 TABLE_NAME = "product_line"
 
 
-def test_model_structure_product_table_exists(db_inspector):
+def test_model_structure_product_line_table_exists(db_inspector):
     assert db_inspector.has_table(TABLE_NAME)
+
+
+def test_model_structure_product_line_column_types(db_inspector):
+    columns = {
+        columns["name"]: columns for columns in db_inspector.get_columns(TABLE_NAME)
+    }
+
+    assert isinstance(columns["id"]["type"], Integer)
+    assert isinstance(columns["price"]["type"], type(Numeric(precision=5, scale=2)))
+    assert isinstance(columns["sku"]["type"], UUID)
+    assert isinstance(columns["stock_qty"]["type"], Integer)
+    assert isinstance(columns["is_active"]["type"], Boolean)
+    assert isinstance(columns["order"]["type"], Integer)
+    assert isinstance(columns["weight"]["type"], Float)
+    assert isinstance(columns["product_id"]["type"], Integer)

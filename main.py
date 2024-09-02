17c44c64ccc08fa5a3b18155e41be3a330890234
chandanch <chandan.cec107@gmail.com>
2024-09-02 16:59:56 +0530

    setup of logging with log conf

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+import logging
+import logging.config
+
 app = FastAPI()
+
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 
 @app.get("/healthcheck")
 def health_check():
+    logger.debug("Testing API Health")
     return {"status": "Server is Up & Running!"}

eb5e60dc15e8730f21a8accf42082cf7a9673e7e
chandanch <chandan.cec107@gmail.com>
2024-09-04 18:31:03 +0530

    create model factory with ablity to generate random category

---
+++ tests/unit/factories/models_factory.py
@@ -0,0 +1,26 @@
+from faker import Faker
+
+faker = Faker()
+
+
+class Category:
+    def __init__(
+        self, id: int, name: str, slug: str, is_active: bool, level: int, parent_id: int
+    ) -> None:
+        self.id = id
+        self.name = name
+        self.slug = slug
+        self.is_active = is_active
+        self.level = level
+        self.parent_id = parent_id
+
+
+def generate_random_category_as_dict(id: int):
+    return {
+        "id": id or faker.random_int(1, 1999),
+        "name": faker.word(),
+        "slug": faker.slug(),
+        "is_active": faker.boolean(),
+        "level": faker.random_int(1, 20),
+        "parent_id": None,
+    }

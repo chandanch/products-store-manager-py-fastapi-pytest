e4fc9c59681cdb763083d341761a79e9668c7d8b
chandanch <chandan.cec107@gmail.com>
2024-09-09 22:25:16 +0530

    setup of category router

--- app/main.py
+++ app/main.py
@@ -2,14 +2,17 @@ from fastapi import FastAPI
 
 import logging
 import logging.config
+from app.routers.category import category_router
 
 app = FastAPI()
+app.include_router(category_router, prefix="/api/category", tags=["category"])
+
 
 logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
 
-@app.get("/healthcheck")
+@app.get("/healthcheck", tags=["healthcheck"])
 def health_check():
     logger.debug("Testing API Health")
     return {"status": "Server is Up & Running!"}
